51edbc3b22a20a6223612675c2cb6853d5c82182
qninth <zxcv12345mmmm@163.com>
2021-07-19 01:55:59 +0800

    use & bit and 2

--- vnum.py
+++ vnum.py
@@ -4,7 +4,7 @@ Created on Fri Jul 16 23:52:44 2021
 
 @author: q
 """
-
+from functools import reduce
 class Vnum():
     def __init__(self, bits, num):
         assert bits > 0
@@ -20,24 +20,37 @@ class Vnum():
         self.num = int(s, 2)
     
     def __getitem__(self, key):
-        bits = self.bits - 1
-        s = f'{self.num:0{self.bits}b}'
         if isinstance(key, int):
-            if key >= bits or key < -bits:
+            if key >= self.bits or key < -self.bits:
                 raise(IndexError("Vnum index out of range"))
-            elif key >= 0:
-                return s[bits - key]
             else :
-                return s[ -key-1 ]
+                if key >= 0:
+                    return self.num & (2**key)
+                else :
+                    return self.num & (2**(key + self.bits))
         if isinstance(key, slice):
+            def bitAnd (t, x): 
+                return t + (self.num & (2**x))
+
             if key.stop == None and key.step == None:
-                return s[slice(bits - key.start, key.stop, 1)]
-            elif key.start > key.stop and key.step == None:
-                if key.start > bits or key.stop < 0:
+                if key.start >= self.bits or key.start < -self.bits:
                     raise(IndexError("Vnum index out of range"))
-                return s[slice(bits - key.start, bits - key.stop, 1)]
-            else :
-                return s[slice(bits - key.start, bits - key.stop, -1)]
+                if key.start >= 0:
+                    return reduce(bitAnd, range(0, key.start+1), 0) // (2**0)
+                else :
+                    return reduce(bitAnd, range(key.start + self.bits, self.bits), 0) // (2**(key.start + self.bits))
+            elif key.start > key.stop and key.step == None:
+                if key.start > self.bits or key.stop < 0:
+                    raise(IndexError("Vnum index out of range"))
+                return reduce(bitAnd, range(key.stop, key.start + 1), 0) // (2**key.stop)
+            elif key.start == key.stop and key.step == None:
+                return self[key.start]
+            elif key.step == None :
+                if key.stop >= self.bits or key.stop < -self.bits:
+                    raise(IndexError("Vnum index out of range"))
+                return reduce(bitAnd, range(key.start, key.stop + 1), 0) // (2**key.start)
+            else:
+                return reduce(bitAnd, range(key.start, key.stop + 1, key.step), 0) // (2**key.start)
 
 
 v = Vnum(8, 5)
@@ -45,12 +58,19 @@ v = Vnum(8, 5)
 # 7:0 5
 # 00000101
 
+# int
 print(v[-1]) # 0
 print(v[0]) # 1
 print(v[1]) # 0
 # print(v[8]) # index error
-print(v[2:0]) # 10
-print(v[1:4]) # 010
-print(v[7:]) # 00000101
 
+# slice
+print(v[7:]) # [7:0] 00000101
+print(v[-1:]) # [-1:0] 0
+print(v[-7:]) # 0000010
+print(v[-8:]) # 00000101
 
+print(v[2:0]) # 101
+print(v[2:1]) # 10
+
+print(v[1:4]) # 0010
