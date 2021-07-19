07c2a1e137d4b328482a3764b7836358c9865453
qninth <zxcv12345mmmm@163.com>
2021-07-19 17:09:29 +0800

    Update vnum.py

--- vnum.py
+++ vnum.py
@@ -10,6 +10,7 @@ class Vnum():
         assert bits > 0
         self.bits = bits
         self.num = num
+        self.w = 0
         
     def parseSlice(self,slc):
         pass
@@ -21,6 +22,7 @@ class Vnum():
     
     def __getitem__(self, key):
         if isinstance(key, int):
+            self.w = 1;
             if key >= self.bits or key < -self.bits:
                 raise(IndexError("Vnum index out of range"))
             else :
@@ -28,11 +30,13 @@ class Vnum():
                     return self.num & (2**key)
                 else :
                     return self.num & (2**(key + self.bits))
+                
         if isinstance(key, slice):
             def bitAnd (t, x): 
                 return t + (self.num & (2**x))
 
             if key.stop == None and key.step == None:
+                self.w = key.start+1;
                 if key.start >= self.bits or key.start < -self.bits:
                     raise(IndexError("Vnum index out of range"))
                 if key.start >= 0:
@@ -40,18 +44,28 @@ class Vnum():
                 else :
                     return reduce(bitAnd, range(key.start + self.bits, self.bits), 0) // (2**(key.start + self.bits))
             elif key.start > key.stop and key.step == None:
+                self.w = key.start-key.stop+1;
                 if key.start > self.bits or key.stop < 0:
                     raise(IndexError("Vnum index out of range"))
                 return reduce(bitAnd, range(key.stop, key.start + 1), 0) // (2**key.stop)
             elif key.start == key.stop and key.step == None:
                 return self[key.start]
             elif key.step == None :
-                if key.stop >= self.bits or key.stop < -self.bits:
+                self.w = key.stop-key.start+1;
+                if key.stop >= self.bits or key.start < 0:
                     raise(IndexError("Vnum index out of range"))
                 return reduce(bitAnd, range(key.start, key.stop + 1), 0) // (2**key.start)
             else:
                 return reduce(bitAnd, range(key.start, key.stop + 1, key.step), 0) // (2**key.start)
-
+            
+        if isinstance(key, list):
+            s = ''
+            for k in key:
+                n = self[k]
+                w = self.w
+                s += f'{n:0{w}b}'
+            return int(s, 2)
+                
 
 v = Vnum(8, 5)
 
